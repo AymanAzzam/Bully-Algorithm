20011e5b5e2fe83b3bf41fe81c6dc600b036a6a2
Ayman Azzam <aymanazzam63@gmail.com>
2020-03-21 20:06:06 +0200

    read configuration file is done

--- bully.py
+++ bully.py
@@ -4,9 +4,21 @@ my_ip_port = ""
 
 leader_ip_port = ""
 
+time_limit = int ## in seconds
+
+def print_dec():
+    for k,v in dec.items():
+        print(k,v)
 
 def init():
-    return 
+    f = open("config.txt","r")
+    time_limit = int(f.readline())
+    machines_num = int(f.readline())
+    for i in range(0,machines_num):
+        ip_port_pri = (f.readline())
+        dec[ip_port_pri.split(" ")[0]] = ip_port_pri.split(" ")[1]
+    print_dec()
+
 def electLeader():
     return
 def main():
