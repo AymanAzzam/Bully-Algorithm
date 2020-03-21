e3968b1654cdb4051f6677bb7b89d24813466ae8
Ayman Azzam <aymanazzam63@gmail.com>
2020-03-21 21:13:02 +0200

    sockets is done

--- bully.py
+++ bully.py
@@ -1,28 +1,41 @@
-dec = {}
+import sys
+import zmq
 
-my_ip_port = ""
+# leader_time and okay_time in milliseconds
 
-leader_ip_port = ""
-
-time_limit = int ## in seconds
-
-def print_dec():
+def print_dec(dec):
     for k,v in dec.items():
         print(k,v)
 
-def init():
+def configuration():
+    dec = {}
     f = open("config.txt","r")
-    time_limit = int(f.readline())
+    leader_time = int(f.readline())
+    okay_time = int(f.readline())
     machines_num = int(f.readline())
-    for i in range(0,machines_num):
+
+    while(machines_num>0):
         ip_port_pri = (f.readline())
         dec[ip_port_pri.split(" ")[0]] = ip_port_pri.split(" ")[1]
-    print_dec()
+        machines_num -= 1
+    print_dec(dec)
+    return dec,leader_time,okay_time
+
+def connection():
+    context = zmq.Context()
+    pub_sucket = context.socket(zmq.PUB)
+    sub_sucket = context.socket(zmq.SUB)
+
+    pub_sucket.bind("tcp://%s"%my_ip_port)
+    sub_sucket.subscribe("")
+    return pub_sucket,sub_sucket
 
 def electLeader():
     return
+
 def main():
-    init()
+    dec,leader_time,okay_time = configuration()
+    pub_sucket,sub_sucket = connection()
     electLeader()
 
 main()
