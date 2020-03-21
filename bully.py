3037c49986969a3a75e7a5f314927deeda37f500
Ayman Azzam <aymanazzam63@gmail.com>
2020-03-21 23:29:28 +0200

    add task function

--- bully.py
+++ bully.py
@@ -21,22 +21,32 @@ def configuration():
     print_dec(dec)
     return dec,leader_time,okay_time
 
-def connection(my_ip_port):
+def connection(my_ip_port,dec):
     context = zmq.Context()
     pub_sucket = context.socket(zmq.PUB)
     sub_sucket = context.socket(zmq.SUB)
 
     pub_sucket.bind("tcp://%s"%my_ip_port)
     sub_sucket.subscribe("")
+    for k,v in dec.items():
+        if(k != my_ip_port):
+            sub_sucket.connect("tcp://%s"%k)
+    
     return pub_sucket,sub_sucket
 
-def electLeader():
-    return
+def electLeader(my_ip_port):
+    return 0
+
+def task(my_ip_port,leader_ip_port):
+    if(leader_ip_port == my_ip_port):
+        print("I'm the Leader, my_ip_port is %s"%my_ip_port)
+    else:
+        print("I'm normal machine, my_ip_port is %s"%my_ip_port)
 
 def main():
     my_ip_port = sys.argv[1]
     dec,leader_time,okay_time = configuration()
-    pub_sucket,sub_sucket = connection(my_ip_port)
-    electLeader()
+    pub_sucket,sub_sucket = connection(my_ip_port,dec)
+    leader_ip_port = electLeader(my_ip_port)
 
 main()
